ae92da00ed35af7f06edccd64436f531f47aadcc
armen-16945 <80101033+armen-16945@users.noreply.github.com>
2021-07-14 21:18:08 +0400

    bazmapatkman axyusak

--- lesson5/bazmaxyusak.cpp
+++ lesson5/bazmaxyusak.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int num;
-    int sum =0;
+    int sum = 0;
     
     cout << "Enter your number= ";
     cin >> num;
     
     for (int i = 0; i <= 10; ++i)
     {
-        
+       cout << num << "*" << i << "  ";
       sum = num * i;
       cout << sum << endl;
     }
-
+    
 
 }
