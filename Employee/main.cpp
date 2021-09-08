6d7982751c761300dffadeba31800a7db9beebac
armen-16945 <80101033+armen-16945@users.noreply.github.com>
2021-09-08 21:25:02 +0400

    main.cpp

---
+++ Employee/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#define NSIZE 7
+using namespace std;
+class Employee
+{
+        private:
+        int employee_ID;
+        int account;
+        public:
+        void creditAccount(int amount)
+        {
+
+        }
+
+        void debitAccount(int ammont)
+        {
+
+        }
+
+        void setImpoye_ID(int id)
+        {
+                
+        }
+
+        int getBalance()
+        {
+
+        }
+        
+};
+
+int main()
+{
+        Employee staf[NSIZE];
+        for(int i = 1; i < NSIZE; i++)
+        {
+                
+        }
+}
