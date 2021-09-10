31703d0a5f66562ce4f5b9f0ad8f6e0936a0b1eb
armen-16945 <80101033+armen-16945@users.noreply.github.com>
2021-09-10 21:19:41 +0400

    getText()

---
+++ get text/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Parces
+{
+  string text;
+  
+  public:
+  Parces(string atext)
+  {
+      text = atext;
+  }
+  void PrintText();
+  string getText();
+};
+
+void Parces::PrintText()
+{
+   cout << text << endl;
+}
+
+string Parces::getText()
+{
+        return text;
+}
+
+int main()
+{
+         Parces p("Hello Word, I am a student");
+        p.PrintText();
+        int k;
+        k = getText()
+        
+}
